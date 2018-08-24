17370d6b03b4927ee8ec9f4a26b7397455df4e93
Owen Lee <yuwenyun.owen.lee@gmail.com>
2018-08-24 15:33:00 +0800

    Create cluster_health.sh

---
+++ doc/cluster_health.sh
@@ -0,0 +1,11 @@
+# show verbose output
+curl -XGET 'localhost:9200/_cat/master?v'
+# show column specification
+curl -XGET 'localhost:9200/_cat/master?help'
+# show specific columns
+curl -XGET 'localhost:9200/_cat/nodes?h=ip,port,cpu,heap.percent'
+
+# _cat api
+curl -XGET 'localhost:9200/_cat/nodes'
+curl -XGET 'localhost:9200/_cat/health'
+curl -XGET 'localhost:9200/_cat/indices'
